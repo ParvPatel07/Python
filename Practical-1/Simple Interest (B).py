6179b6ca6275dbad6b5c7be765c2251164f71e1a
ParvPatel07 <155556639+ParvPatel07@users.noreply.github.com>
2024-02-10 19:49:34 +0530

    Add files via upload

---
+++ Practical-1/Simple Interest (B).py
@@ -0,0 +1,8 @@
+1
+p = float(input("Enter the principal amount: "))
+r = float(input("Enter the rate of interest: "))
+t = float(input("Enter the time period in years: "))
+
+si = (p * r * t) / 100
+
+print("The simple interest is", si)
